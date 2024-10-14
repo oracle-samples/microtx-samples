0c3fc4df8f89f35a547ef0a5596158df71888980
Janaki Ram Puli <janaki.ram.puli@oracle.com>
2024-10-14 15:07:14 +0530

    MTRM-1879 : OpenFeign client support for LRA samples

--- lra/lrademo/trip-manager-springboot/src/main/java/com/example/tripmanagersb/feignclient/FlightFeignClient.java
+++ lra/lrademo/trip-manager-springboot/src/main/java/com/example/tripmanagersb/feignclient/FlightFeignClient.java
@@ -2,12 +2,13 @@ package com.example.tripmanagersb.feignclient;
 
 import com.example.tripmanagersb.model.Booking;
 import com.oracle.microtx.springboot.lra.clientinterceptor.MicroTxLRAFeignClientInterceptor;
+import com.oracle.microtx.springboot.lra.clientinterceptor.MicroTxLRAFeignClientInterceptorConfig;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(value = "${flight.service.name}", url = "${flight.service.url}", configuration = MicroTxLRAFeignClientInterceptor.class)
+@FeignClient(value = "${flight.service.name}", url = "${flight.service.url}", configuration = MicroTxLRAFeignClientInterceptorConfig.class)
 public interface FlightFeignClient {
     @RequestMapping(method = RequestMethod.POST)
     Booking bookFlight(@RequestParam("flightNumber") String flightNumber);
