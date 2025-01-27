3a2922fdff269c5f16017b13dc115966682fb8ff
Tushar Shaily <tushar.shaily@oracle.com>
2025-01-27 16:24:31 +0530

    Added Sample app for spring boot and helidon with embedded database

---
+++ xa/java/department-helidon-embedded/src/main/java/com/oracle/mtm/sample/entity/Account.java
@@ -0,0 +1,75 @@
+/*
+Copyright (c) 2023, Oracle and/or its affiliates. **
+
+The Universal Permissive License (UPL), Version 1.0 **
+
+Subject to the condition set forth below, permission is hereby granted to any person obtaining a copy of this software, associated documentation and/or data
+(collectively the "Software"), free of charge and under any and all copyright rights in the Software, and any and all patent rights owned or freely licensable by each
+licensor hereunder covering either the unmodified Software as contributed to or provided by such licensor, or (ii) the Larger Works (as defined below), to deal in both **
+(a) the Software, and (b) any piece of software and/or hardware listed in the lrgrwrks.txt file if one is included with the Software (each a "Larger Work" to which the
+Software is contributed by such licensors), **
+without restriction, including without limitation the rights to copy, create derivative works of, display, perform, and distribute the Software and make, use, sell,
+offer for sale, import, export, have made, and have sold the Software and the Larger Work(s), and to sublicense the foregoing rights on either these or other terms. **
+
+This license is subject to the following condition: The above copyright notice and either this complete permission notice or at a minimum a reference to the UPL must be
+included in all copies or substantial portions of the Software. **
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+package com.oracle.mtm.sample.entity;
+
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+
+/**
+ * The Account Entity class.
+ */
+@Schema(name = "Account")
+public class Account {
+    @Schema(required = true, description = "Account identity")
+    String accountId;
+    @Schema(required = true, description = "Account name")
+    String name;
+    @Schema(required = true, description = "Amount associated with the account")
+    double amount;
+
+    public Account(String accountId, String name, double amount) {
+        this.accountId = accountId;
+        this.name = name;
+        this.amount = amount;
+    }
+
+    public void setAccountId(String accountId) {
+        this.accountId = accountId;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public String getAccountId() {
+        return accountId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    @Override
+    public String toString() {
+        return "Account{" +
+                "accountId='" + accountId + '\'' +
+                ", name='" + name + '\'' +
+                ", amount=" + amount +
+                '}';
+    }
+}
